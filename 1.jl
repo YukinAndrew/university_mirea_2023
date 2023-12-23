fda9b7e31c844a561143325e38ce4209f27da8e1
YukinAndrew <77924490+YukinAndrew@users.noreply.github.com>
2023-12-23 14:23:18 +0300

    Update 1.jl

--- 1.jl
+++ 1.jl
@@ -1,3 +1,12 @@
+"""
+ДАНО: робот находится в произвольной клетке ограниченного 
+прямоугольного поля (без внутренних перегородок и маркеров).
+РЕЗУЛЬТАТ: робот - в исходном положении в центре из прямого креста из 
+маркеров.
+"""
+include("robot.jl")
+include("functions.jl")
+
 function mark_cross!(robot)::Nothing
     putmarker!(robot)
     for side in 0:3
@@ -6,13 +15,13 @@ function mark_cross!(robot)::Nothing
 end
 
 function line_markings!(robot, direction)
-    local num_steps = 0
+    local steps = 0
     while !isborder(robot, direction)
         move!(robot, direction)
-        num_steps += 1
+        steps += 1
         putmarker!(robot)
     end
-    along!(robot, inverse(direction), num_steps)
+    along!(robot, inverse(direction), steps)
 end
 
 print(mark_cross!(r))
