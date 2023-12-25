3921d528d153138c3ba08026c7c129a3e278d91b
YukinAndrew <77924490+YukinAndrew@users.noreply.github.com>
2023-12-25 13:00:47 +0300

    17.jl

---
+++ 17.jl
@@ -0,0 +1,23 @@
+"
+Решить задачу 8 с использованием обобщённой функции 
+spiral!(stop_condition::Function, robot)
+"
+
+using HorizonSideRobots
+include("functions.jl")
+include("structs.jl")
+
+r = Robot(animate = true)
+
+
+function spiral!(stop_condition::Function, robot)
+    n = 2
+    side = Nord
+    while !stop_condition()
+        num_steps_along!(stop_condition, robot, side, div(n, 2))
+        n += 1
+        side = right(side)
+    end
+end
+
+spiral!(() -> ismarker(r), r)
