29dd7f80c8dcf058b6cf178de39c8b8da53834e0
YukinAndrew <77924490+YukinAndrew@users.noreply.github.com>
2023-12-25 13:17:08 +0300

    20.jl

---
+++ 20.jl
@@ -0,0 +1,24 @@
+"
+Написать рекурсивную функцию, перемещающую робота до упора в 
+заданном направлении, ставящую возле перегородки маркер и возвращающую 
+робота в исходное положение.
+"
+
+using HorizonSideRobots
+include("functions.jl")
+
+r = Robot("untitled.sit", animate = true)
+
+function recursion_mark_along!(robot, side)
+    if !isborder(robot, side)
+        move!(robot, side)
+        recursion_mark_along!(robot, side)
+        move!(robot, inverse(side))
+    else
+        putmarker!(robot)
+    end
+end
+
+for side in (Nord, West, Sud, Ost)
+    recursion_mark_along!(r, side)
+end
