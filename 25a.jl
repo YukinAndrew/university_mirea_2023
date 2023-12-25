851b66fe8e3cc4ce25816bf8ce38430ca59cd28e
YukinAndrew <77924490+YukinAndrew@users.noreply.github.com>
2023-12-25 13:22:55 +0300

    25a.jl

---
+++ 25a.jl
@@ -0,0 +1,34 @@
+"
+Написать рекурсивную функцию, перемещающую робота в заданном 
+направлении до упора и расставляющую маркеры в шахматном порядке, 
+a) начиная с установки маркера.
+"
+
+using HorizonSideRobots
+include("functions.jl")
+
+r = Robot(11, 12, animate = true)
+
+"""checkerboard_line!(robot, side) - косвенная рекурся перемещающая робота до упора,
+в заданном направлении, и параллельно ставит маркеры в шахматном порядке, начиная с первой клетки.
+"""
+# checkerboard_line - шахматная линия
+function checkerboard_line!(robot, side)
+    if !isborder(robot, side)
+        putmarker!(robot)
+        move!(robot, side)
+        no_delated_action!(robot, side)
+    else
+        putmarker!(robot)
+    end
+end
+
+function no_delated_action!(robot, side)
+    if !isborder(robot, side)
+        move!(robot, side)
+        checkerboard_line!(robot, side)
+    end
+end
+
+checkerboard_line!(r, Nord)
+checkerboard_line!(r, Ost)
