6ba5e358ebb46db15e4e8dc64f142cdcb80eab23
YukinAndrew <77924490+YukinAndrew@users.noreply.github.com>
2023-12-25 12:59:20 +0300

    15.jl

---
+++ 15.jl
@@ -0,0 +1,31 @@
+"
+Решить задачу 4, но при условии наличия на поле простых внутренних 
+перегородок.
+"
+
+using HorizonSideRobots
+HSR = HorizonSideRobots
+include("functions.jl")
+include("structs.jl")
+
+r = Robot("untitled15.sit", animate = true)
+
+function mark_line!(robot, side)
+    while try_move!(RectBorderRobot{Robot}(robot), side)
+        putmarker!(robot)
+    end
+    while try_move!(RectBorderRobot{Robot}(robot), inverse(side))
+        if !ismarker(robot)
+            return
+        end
+    end
+end
+
+function mark_cross_x!(robot)
+    for side in ((Nord, Ost), (Nord, West), (Sud, West), (Sud, Ost))
+        mark_line!(robot, side)
+    end
+    putmarker!(robot)
+end
+
+mark_cross_x!(r)
